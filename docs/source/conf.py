32b113c4ef6188df4882b8db3e4516315400b4e3
Jianyu Yang <yztxwd@gmail.com>
2023-03-20 22:28:14 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ from unittest import mock
 MOCK_MODULES = ["numpy",
                 "pandas",
                 "torch",
+                "torch.utils.data",
                 "pysam",
                 "pybedtools",
                 "pyfasta",
